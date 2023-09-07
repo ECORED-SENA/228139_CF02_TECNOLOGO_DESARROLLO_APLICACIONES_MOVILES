c37aa75c8dd6ccb3afc1ac93438528f95fec169b
Camilo Bolaño <80546125+elrilacho@users.noreply.github.com>
2023-09-07 16:05:36 -0500

    Video_cargado

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -12,10 +12,10 @@
       img(src="@/assets/curso/intro/img01.png", alt="Introducción")
     
     p.mb-5 En este componente formativo se estudiarán los principios básicos sobre los que se fundamenta el proceso de análisis de requisitos desde marcos de desarrollo de #[em software] tradicional y ágiles. También, introduce a diferentes formas (planillas y estándares) para realizar el proceso de documentación de requisitos. El siguiente video expone, de forma genérica lo que se desarrollará de esta temática.
-    //- figure
-    //-   .video
-    //-     iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-    //-   figcaption Video. 
+    figure
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/Hmtriz7Af20?si=vXLGQJnAplStjO2r" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      figcaption Video. Análisis y especificación de requisitos
 </template>
 
 <script>
