dd18b33b471621119bc35fd2541b5e63aa25ae3e
Camilo Bolaño <80546125+elrilacho@users.noreply.github.com>
2023-09-12 07:29:44 -0500

    Material_cargadado

--- src/config/global.js
+++ src/config/global.js
@@ -135,7 +135,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/228139_CF02_DU.zip',
       },
       {
         icono: 'fas fa-download',
